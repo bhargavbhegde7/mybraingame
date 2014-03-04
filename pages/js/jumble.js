e6c6561390381730b850ad5361a062a4877c038e
bhargav <bhargavbhegde7@gmail.com>
2014-03-04 16:55:55 +0530

    switch instead of for loop

--- pages/js/jumble.js
+++ pages/js/jumble.js
@@ -1,6 +1,6 @@
-var pattern=new Array(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
+var pattern=new Array();
 var i=5,count=0,clickPermission=false;
-var clickTimer=0;
+var clickTimer=0,endClickTimer=false;
 function levelMore()
 {
 i++;
@@ -8,26 +8,61 @@ i++;
 function getImg(imgId)
 {
 	if(clickPermission)
-	{
-		document.getElementById(imgId).style.background="red";
-		for(var y=i;y<=25;y++)
+	{		
+		switch(pattern[imgId-1])
 		{
-			if(imgId==pattern[y])
-			{
-				count=0;
-				//alert("game over");
-				Avgrund.show( "#wrong-popup" );
-				clickPermission=false;
-			}
+			case 1:
+				document.getElementById(imgId).style.background="red";
+				//document.getElementById(imgId).innerHTML=imgId;
+				count++;
+				document.getElementById("countTag").innerHTML=i-count;
+				clickTimer=0;
+				pattern[imgId-1]=-1; //turn it into clicked correct one. ("-1")
+			break;
+			case 0:
+				count++;
+				document.getElementById("countTag").innerHTML=i-count;
+				document.getElementById(imgId).style.background="red";
+				pattern[imgId-1]=2; //turn it into clicked wrong one. ("2")
+			break;
+			case -1:
+				//if that is one of the correct ones				
+				count--;
+				document.getElementById("countTag").innerHTML=i-count;
+				document.getElementById(imgId).style.background="black";
+				pattern[imgId-1]=1; //turn it into unclicked correct one. ("1")
+			break;
+			case 2:
+				//of if it was one of the wrong ones
+				count--;
+				document.getElementById("countTag").innerHTML=i-count;		
+				document.getElementById(imgId).style.background="black";
+				pattern[imgId-1]=0;//turn it into unclicked wrong one. ("0")
+			break;
 		}
-		count++;
-			if(count>=i)
+		//once all the clicking is over
+		if(count>=i)
+		{
+			for(var y=0;y<25;y++) // check for a wrong tile clicked
 			{
-				count=0;
-				//alert("correct");
-				Avgrund.show( "#correct-popup" );
-				clickPermission=false;
+				if(pattern[y]==2) // if there is a clicked wrong ("2")
+				{
+					count=0;					
+					document.getElementById("countTag").innerHTML="--";
+					document.getElementById("wrong-message").style.display="inline";
+					document.getElementById("wrong-message").innerHTML="wrong tiles detected";
+					Avgrund.show( "#wrong-popup" );
+					clickPermission=false;
+					endClickTimer=true;
+					return;
+				}
 			}
+			count=0;
+			document.getElementById("countTag").innerHTML="--";
+			Avgrund.show( "#correct-popup" );
+			clickPermission=false;
+			endClickTimer=true;
+		}		
 	}
 }
 function resetGrid()
@@ -39,6 +74,7 @@ function resetGrid()
 }
 function makePattern()
 {
+	document.getElementById("countTag").innerHTML=i-count;
 	for(var n=0;n<25;n++)
 	{
 		document.getElementById(n+1).style.background="black";
@@ -46,28 +82,58 @@ function makePattern()
 	}	
 	var m;	
 	var j=0;
-	while(j<=25)
+	while(j<i)//select 'i' elements randomly from 'pattern' array and make them true
 	{
 		var rndmNum = Math.floor ((Math.random () * 100) % 25);		
 		if(!pattern[rndmNum])
 		{			
-			pattern[rndmNum]=j++;
+			//pattern[rndmNum]=j++;
+			pattern[rndmNum]++;
+			j++;
 		}		
  	}	
-	for(m=0;m<i;m++)
+	for(m=0;m<=25;m++)
 	{
-		document.getElementById(pattern[m]).style.background="red";
-		//document.getElementById(pattern[m]).style.setAttribute("style","background-color: yellow;");
-		pattern[m]=-1;
+		if(pattern[m])
+		{
+			document.getElementById(m+1).style.background="red";			
+		}
 	}
 	setTimeout(function(){
 		for(var n=0;n<25;n++)
 		{
-			document.getElementById(n+1).style.background="black";
+			document.getElementById(n+1).style.background="black";			
 		}
+		clickTimer=0;
+		startClickTimer();
 		clickPermission=true;
 	},1000);
-	setTimeout(function(){
-		
+}
+function startClickTimer()
+{
+	if(endClickTimer)
+	{
+		endClickTimer=false;
+		clickTimer=0;
+		document.getElementById("clickTimerTag").innerHTML="--";
+		return;
+	}
+	if(clickTimer>=5)
+	{
+		count=0;		
+		document.getElementById("wrong-message").style.display="inline";
+		document.getElementById("wrong-message").innerHTML="timeout";
+		Avgrund.show( "#wrong-popup" );
+		clickPermission=false;
+		return;
+	}
+	setTimeout(function(){		
+		clickTimer+=1;
+		document.getElementById("clickTimerTag").innerHTML=clickTimer;
+		startClickTimer();
 	},1000);
+}
+function printArray()
+{
+	document.write(pattern);
 }
